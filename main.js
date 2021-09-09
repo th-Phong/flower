3751e6bf6c449cae353aa1eeac3415d4be8325dc
th-Phong <hongphong120300@gmail.com>
2021-09-09 15:34:20 +0700

    first commit

---
+++ main.js
@@ -0,0 +1,95 @@
+const $$ = document.querySelectorAll.bind(document);
+const $ = document.querySelector.bind(document);
+const PLAYER_KEY = "Flower";
+const audio = $("#audio");
+const player = $(".player");
+const btnPlay = $(".btn-toggle-play");
+const progress = $("#progress");
+const btnRepeat = $(".btn-repeat");
+const cdThumb = $(".cd-thumb");
+
+const app = {
+  isRepeat: false,
+  isPlaying: false,
+  currentIndex: 0,
+  config: JSON.parse(localStorage.getItem(PLAYER_KEY)) || {},
+  setConfig: function (key, value) {
+    this.config[key] = value;
+    localStorage.setItem(PLAYER_KEY, JSON.stringify(this.config));
+  },
+  songs: [
+    {
+      name: "Muộn rồi mà sao còn",
+      singer: "Sơn Tùng M-TP",
+      path: "https://aredir.nixcdn.com/Believe_Audio19/MuonRoiMaSaoCon-SonTungMTP-7011803.mp3?st=w9AA-eyRI7yD_VYGfvVWeQ&e=1623141624",
+      image: "https://pbs.twimg.com/media/Ez5jRyVVgAQN6Kh.jpg",
+    },
+  ],
+
+  handleEvents: function () {
+    const _this = this;
+
+    //xử lý khi play
+    btnPlay.onclick = function () {
+      if (_this.isPlaying) {
+        audio.pause();
+      } else {
+        audio.play();
+      }
+    };
+
+    //xử lý khi repeat
+    btnRepeat.onclick = function () {
+      _this.isRepeat = !_this.isRepeat;
+      _this.setConfig("isRepeat", _this.isRepeat);
+      btnRepeat.classList.toggle("active", _this.isRepeat);
+    };
+    audio.onplay = function () {
+      _this.isPlaying = true;
+      player.classList.add("playing");
+    };
+    audio.onpause = function () {
+      _this.isPlaying = false;
+      player.classList.remove("playing");
+    };
+    audio.ontimeupdate = function () {
+      if (audio.duration) {
+        const progressPercent = (audio.currentTime / audio.duration) * 100;
+        progress.value = progressPercent;
+      }
+    };
+    progress.oninput = function (e) {
+      const seekTime = (audio.duration / 100) * e.target.value;
+      audio.currentTime = seekTime;
+    };
+    audio.onended = function () {
+      if (_this.isRepeat) {
+        audio.play();
+      } else {
+        btnNext.click();
+      }
+    };
+  },
+
+  loadConfig: function () {
+    this.isRepeat = this.config.isRepeat;
+  },
+  defineProperty: function () {
+    Object.defineProperty(this, "currentSong", {
+      get: function () {
+        return this.songs[this.currentIndex];
+      },
+    });
+  },
+  loadCurrentSong: function () {
+    audio.src = this.currentSong.path;
+  },
+  start: function () {
+    this.loadConfig();
+    btnRepeat.classList.toggle("active", this.isRepeat);
+    this.defineProperty();
+    this.handleEvents();
+    this.loadCurrentSong();
+  },
+};
+app.start();
